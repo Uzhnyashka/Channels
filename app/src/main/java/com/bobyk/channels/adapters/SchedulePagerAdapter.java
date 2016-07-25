3db431ddeffcd5d15609f43d1e40dcf5c5c3afd5
EgorBobyk <egor4k.964@gmail.com>
2016-07-25 14:08:46 +0300

    FragmentPagerAdapter

--- app/src/main/java/com/bobyk/channels/adapters/SchedulePagerAdapter.java
+++ app/src/main/java/com/bobyk/channels/adapters/SchedulePagerAdapter.java
@@ -2,30 +2,42 @@ package com.bobyk.channels.adapters;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bobyk.channels.ChannelContract;
 import com.bobyk.channels.R;
+import com.bobyk.channels.fragments.PageFragment;
 
 /**
  * Created by bobyk on 25/07/16.
  */
-public class SchedulePagerAdapter extends PagerAdapter {
+public class SchedulePagerAdapter extends FragmentPagerAdapter {
 
     private Cursor cursor;
     private LayoutInflater layoutInflater;
 
-    public SchedulePagerAdapter(Context context, Cursor cursor){
+    public SchedulePagerAdapter(Context context, FragmentManager fm, Cursor cursor) {
+        super(fm);
+        this.cursor = cursor;
+    }
+
+ /*   public SchedulePagerAdapter(Context context, Cursor cursor){
+        super(context, cursor);
         this.cursor = cursor;
         this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
+    }*/
 
     @Override
     public int getCount() {
@@ -34,21 +46,29 @@ public class SchedulePagerAdapter extends PagerAdapter {
         else return cursor.getCount();
     }
 
-    @Override
+    /*@Override
     public boolean isViewFromObject(View view, Object object) {
         return view == object;
-    }
+    }*/
 
     public void swapCursor(Cursor cursor){
         this.cursor = cursor;
+        notifyDataSetChanged();
     }
 
-    @Override
+  /*  @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        ((ViewPager)container).removeView((LinearLayout)object);
-    }
+        cursor.moveToPosition(position);
+        (container).removeView((View) object);
+    }*/
 
     @Override
+    public Fragment getItem(int position) {
+        Log.d("bla", "Bla " + position);
+        return PageFragment.newInstance(position);
+    }
+
+  /*  @Override
     public Object instantiateItem(ViewGroup container, int position) {
         position = position % cursor.getCount();
 
@@ -59,7 +79,14 @@ public class SchedulePagerAdapter extends PagerAdapter {
         TextView tvChannel = (TextView) layout.findViewById(R.id.channel_tv);
         tvChannel.setText(cursor.getString(cursor.getColumnIndex(ChannelContract.ChannelEntry.COLUMN_NAME)));
 
-        ((ViewPager)container).addView(layout);
+
+        (container).addView(layout);
         return layout;
+    }*/
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        cursor.moveToPosition(position);
+        return cursor.getString(cursor.getColumnIndex(ChannelContract.ChannelEntry.COLUMN_NAME));
     }
 }
--- app/src/main/java/com/bobyk/channels/fragments/ScheduleFragment.java
+++ app/src/main/java/com/bobyk/channels/fragments/ScheduleFragment.java
@@ -5,14 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bobyk.channels.ChannelAdapter;
+import com.bobyk.channels.ChannelContract;
+import com.bobyk.channels.ChannelDBHelper;
+import com.bobyk.channels.MainActivity;
 import com.bobyk.channels.R;
+import com.bobyk.channels.adapters.SchedulePagerAdapter;
 
 /**
  * Created by bobyk on 24/07/16.
@@ -20,26 +26,39 @@ import com.bobyk.channels.R;
 public class ScheduleFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private ViewPager channelPager;
-    private ChannelAdapter channelAdapter;
+    private SchedulePagerAdapter schedulePagerAdapter;
+    private ChannelDBHelper channelDbHelper;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.channel_schedule, null);
         channelPager = (ViewPager) view.findViewById(R.id.viewPager);
+        channelPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d("bla","Page " + position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         return view;
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
+        channelDbHelper = ChannelDBHelper.getInstance(getActivity());
+        schedulePagerAdapter = new SchedulePagerAdapter(getActivity(), getFragmentManager(), null);
+        channelPager.setAdapter(schedulePagerAdapter);
     }
 
     public static ScheduleFragment newInstance() {
@@ -52,16 +71,33 @@ public class ScheduleFragment extends Fragment implements LoaderManager.LoaderCa
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+        return new CursorLoader(getActivity(), ChannelContract.ChannelEntry.CONTENT_URI, null, null, null, null);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+        schedulePagerAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
+        schedulePagerAdapter.swapCursor(null);
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (channelDbHelper != null){
+            channelDbHelper.close();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (channelDbHelper == null){
+            channelDbHelper = ChannelDBHelper.getInstance(getActivity());
+        }
+        getLoaderManager().initLoader(-1, null, this);
     }
 }
